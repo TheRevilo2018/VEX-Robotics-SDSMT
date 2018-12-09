1fd1f62219ac05055d232b26461ebb17208101b0
Geniusammyr <geniusammyralltheothernamesare@gmail.com>
2018-12-08 17:55:52 -0700

    Added very beginnings of an arcade drive

---
+++ VEXU2018-2019/Coshmo/Pre-test code.cpp
@@ -0,0 +1,116 @@
+#include "robot-config.h"
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*        Description: Competition template for VCS VEX V5                   */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+//Creates a competition object that allows access to Competition methods.
+vex::competition    Competition;
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the cortex has been powered on and    */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton( void ) {
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+  
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous( void ) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+
+}
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*                              User Control Task                             */
+/*                                                                            */
+/*  This task is used to control your robot during the user control phase of  */
+/*  a VEX Competition.                                                        */
+/*                                                                            */
+/*  You must modify the code to add your own robot specific commands here.    */
+/*----------------------------------------------------------------------------*/
+
+void usercontrol( void ) {
+  // User control code here, inside the loop
+  int x;
+  int y;
+  int highThresh = 100;
+  int lowThresh = 20;
+  double percentOnLeft;
+  double percentOnRight;
+  while (1){
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo 
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to 
+    // update your motors, etc.
+    // ........................................................................
+    
+    //determine % motor from controller
+    if (CONTROLLER_VALUE_UD < lowThresh)
+    {
+      percentOnRight = 0.0;
+      percentOnLeft = percentOnRight;
+    }
+    else if (CONTROLLER_VALUE_UD < highThresh)
+    {
+      percentOnRight = fabs((double)CONTROLLER_VALUE / 127);
+      percentOnLeft = percentOnRight;
+    }
+    else //might not be needed, sets a maxout value
+    {
+      percentOnRight = MAXOUT;
+      percentOnLeft = percentOnRight;
+    }
+
+    //determine turn amount from controller
+
+
+    LeftMotor.setVelocity(50, vex::velocityUnits::pct);
+    RightMotor.setVelocity(50, vex::velocityUnits::pct);
+
+    vex::task::sleep(20); //Sleep the task for a short amount of time to prevent wasted resources. 
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+    
+    //Run the pre-autonomous function. 
+    pre_auton();
+    
+    //Set up callbacks for autonomous and driver control periods.
+    Competition.autonomous( autonomous );
+    Competition.drivercontrol( usercontrol );
+
+    //Prevent main from exiting with an infinite loop.                        
+    while(1) {
+      vex::task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
+    }    
+       
+}
