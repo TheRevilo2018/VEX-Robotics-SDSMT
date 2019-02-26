22aeb97d1f81fbd9a7ccf23f284733d61d05cea3
7440034 <matthew.reff@mines.sdsmt.edu>
2019-02-25 19:58:42 -0700

    Added header guard to helperFunctions.h

--- VEXU2018-2019/Coshmo/pros code/src/helperFunctions.h
+++ VEXU2018-2019/Coshmo/pros code/src/helperFunctions.h
@@ -1,6 +1,11 @@
+#ifndef HELPER_FUNCTIONS
+#define HELPER_FUNCTIONS
+
 #include "main.h"
 
 
 void setMotors(std::vector<pros::Motor> & motors, double speed);
 void setBrakes(std::vector<pros::Motor> & motors,  pros::motor_brake_mode_e_t brakeType);
 void doubleLaunch(pros::Motor & launchMotorLeft, pros::Motor & launchMotorRight, pros::Motor & anglerMotor, std::vector<pros::Motor> & intakeMotors);
+
+#endif
